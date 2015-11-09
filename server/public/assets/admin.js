eea7098c41e4a0405a4b91f23b15a259cef35c4d
User <user@Kates-MacBook-Pro.local>
2015-11-09 17:19:49 -0600

    submit working with angular, delete still not working

--- server/public/assets/admin.js
+++ server/public/assets/admin.js
@@ -1,80 +1,37 @@
-$(document).ready(function(){
+var messageApp = angular.module('messageApp', []);
 
-    $('#userMessage').on('click', '.submit', addMessage);
-//the delete button stores the id for the deleted item in the deletedMessage variable. That is then sent the DELETE call further down. Again, the data is displayed minus the deleted item.
-    $('#leftMessages').on('click', '.delete', function(){
-        $(this).parent().remove();
-        var deletedMessage = $(this).data("id");
-        deleteMessage(deletedMessage);
-        displayData();
-    });
+messageApp.controller("MessageController", ['$scope', '$http', function($scope, $http){
+    $scope.values= {};
+    $scope.peopleArray = [];
+    $scope.numbers = {};
 
-    displayData();
-});
+    $scope.clickButton = function(values){
+        console.log($scope.values);
 
-function addMessage() {
-    event.preventDefault();
-    var values = {};
+        //Post to the message from the form to the database.
+        $http.post('/messages', values).then(function(response){
+            console.log("Okay, posted to the database. Check robo");
+            $scope.addMessage();
+        });
+    };
 
-    $.each($("#userMessage").serializeArray(), function(i,field){
-        values[field.name] = field.value;
-    });
+    $scope.deleteButton = function(id){
+       console.log(id);
 
-    console.log(values);
-    $('#name').val("");
-    $('#messageContent').val("");
-    //$("#userMessage").find("input[type=text]").val("");
+    };
 
-    $.ajax({
-        type: 'POST',
-        url: '/messages',
-        data: values,
-        beforeSend: function(){
-            console.log("This is before send: ", values);
-        },
-        success: function(){
-            console.log("Ajax Call POST success");
-            displayData();
-        }
-    });
-}
+    $scope.addMessage = function() {
+        $http.get('/board').then(function(response) {
+            $scope.peopleArray = response.data;
+            console.log($scope.peopleArray);
+        });
 
-function displayData(){
-    $.ajax({
-        type: 'GET',
-        url: '/board',
-        success: function(data){
-            console.log("This is GET data: ", data);
-            appendDOM(data);
-        }
-    });
-}
-//leads to the delete route server side.
-function deleteMessage(deletedMessage){
-    var id = {"id" : deletedMessage};
-    console.log("This is delete message going into ajax: ", deletedMessage);
-    $.ajax({
-        type: 'DELETE',
-        url: '/delete',
-        data: id,
-        success: function(){
-            console.log("Message deleted");
-        }
+    };
 
-    });
-}
+    $scope.deleteMessage = function(){
+        $http.delete('/delete').then(function(response){
 
-function appendDOM(data){
-    $('#leftMessages').empty();
-
-    for(var i= 0; i<data.length; i++){
-        var id = data[i]._id;
-        $('#leftMessages').append("<div class='message-box well' data-id='" +
-            id + "'></div>");
-        var $el=$('#leftMessages').children().last();
-
-        $el.append('<p class="submitted-message">' + data[i].message + '</p>');
-        $el.append('<p class="submitted-name">' + data[i].name + '</p>');
-        $el.append("<button class='delete btn btn-danger' data-id='" + id + "'>Delete</div>");
+        });
     }
-}
+    $scope.addMessage();
+}]);
--- server/public/assets/app.js
+++ server/public/assets/app.js
@@ -1,60 +1,29 @@
-$(document).ready(function(){
-//when the submit button is clicked, it triggers addMessage, which is the post call to the server to add the message in the form to the sever.
-    $('#userMessage').on('click', '.submit', addMessage);
+var messageApp = angular.module('messageApp', []);
 
-    displayData();//displayData is a get call that calls for all messages current stored in the server.
-});
+messageApp.controller("MessageController", ['$scope', '$http', function($scope, $http){
+    $scope.values= {};
+    $scope.peopleArray = [];
 
-function addMessage() {
-    event.preventDefault();
+    $scope.clickButton = function(values){
+        console.log($scope.values);
 
-    var values = {};
+        //Post to the message from the form to the database.
+        $http.post('/messages', values).then(function(response){
+            console.log("Okay, posted to the database. Check robo");
+            $scope.addMessage();
+        });
+    };
 
-    $.each($("#userMessage").serializeArray(), function(i,field){
-        values[field.name] = field.value;
-    });
 
-    console.log(values);
-    $('#name').val("");
-    $('#messageContent').val("");
-    //$("#userMessage").find("input[type=text]").val("");
+    $scope.addMessage = function() {
+        $http.get('/board').then(function(response) {
+            $scope.peopleArray = response.data;
+            console.log($scope.peopleArray);
+        });
 
-    $.ajax({
-        type: 'POST',
-        url: '/messages',
-        data: values,
-        beforeSend: function(){
-            console.log("This is before send: ", values);
-        },
-        success: function(){
-            console.log("Ajax Call POST success");
-            displayData();
-        }
-    });
-}
+    };
+    $scope.addMessage();
+}]);
 
-function displayData(){
-    $.ajax({
-        type: 'GET',
-        url: '/board',
-        success: function(data){
-            console.log("This is GET data: ", data);
-            appendDOM(data);
-        }
-    });
-}
 
-//appends any data sent from displayData. First action is to empty the messages area so that there is no duplication.
-function appendDOM(data){
-    $('#leftMessages').empty();
 
-    for(var i= 0; i<data.length; i++){
-        var id = data[i]._id;
-        $('#leftMessages').append("<div class='message-box well' data-id='" +
-            id + "'></div>");
-        var $el=$('#leftMessages').children().last();
-
-        $el.append('<p class="submitted-message">' + data[i].message + '</p>');
-        $el.append('<p class="submitted-name">' + data[i].name + '</p>');
-    }
-}
